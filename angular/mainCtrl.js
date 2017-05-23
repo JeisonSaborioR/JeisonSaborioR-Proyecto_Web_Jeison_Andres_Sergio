1f2be1ff74193a227c592ba9b24fd402351b4fe2
JeisonSaborioR <jurses93@gmail.com>
2017-05-23 00:20:14 -0600

    mostrar ofertas

--- angular/mainCtrl.js
+++ angular/mainCtrl.js
@@ -1,9 +1,12 @@
 angular.module('mainApplication', [])
 
 .controller('mainCtrl', function($scope, $http) {
-	
+
+	$scope.posts = [];
+
 	$http.get('/titicupones').then(function(data) {
-		console.log(data)
-		$scope.test = data.data
+		//console.log(data)
+		$scope.posts = data.data.titicupones;
+
 	});
 });
