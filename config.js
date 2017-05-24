418641d142256b4f7b4b12341983ebec9c81b7a7
JeisonSaborioR <jurses93@gmail.com>
2017-05-24 16:35:46 -0600

    first commit

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	port: process.env.PORT || 100,
-	db: 'mongodb://localhost/OfertasJSA',
+	db: 'mongodb://jsa:12345@ds149800.mlab.com:49800/heroku_m3hcw5sl'||'mongodb://localhost/OfertasJSA',
 	TOKEN_SECRETO: '256348ofertasparatodoelmundo'
 	//'mongodb://jeison:jeison@ds143231.mlab.com:43231/heroku_f78gvhjp' 
 }
