b083648b80409358d5afa646b17db8ea972f6a6b
JeisonSaborioR <jurses93@gmail.com>
2017-05-24 22:11:30 -0600

    port localhost

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	port: process.env.PORT || 100,
+	port: process.env.PORT || 8080,
 	db: 'mongodb://jsa:12345@ds149800.mlab.com:49800/heroku_m3hcw5sl'||'mongodb://localhost/OfertasJSA',
 	TOKEN_SECRETO: '256348ofertasparatodoelmundo'
 	//'mongodb://jeison:jeison@ds143231.mlab.com:43231/heroku_f78gvhjp' 
